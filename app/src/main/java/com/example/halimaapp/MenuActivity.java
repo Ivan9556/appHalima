8f8777b1aaea6b4dfb23a163b4fd1fe1e679e724
Ivan9556 <168356668+Ivan9556@users.noreply.github.com>
2025-09-18 01:28:40 +0200

    NavegationView, Toolbar and Style

--- app/src/main/java/com/example/halimaapp/MenuActivity.java
+++ app/src/main/java/com/example/halimaapp/MenuActivity.java
@@ -12,17 +12,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import com.example.halimaapp.databinding.ActivityMainBinding;
+import com.example.halimaapp.databinding.ActivityMenuBinding;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MenuActivity extends AppCompatActivity {
 
+    private ActivityMenuBinding binding;
+
     @Override
-    protected void onCreate(Bundle saveInstanceState){
-        super.onCreate(saveInstanceState);
-        setContentView(R.layout.activity_menu);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        MaterialToolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
 
+        // Obtenemos NavController correctamente
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
+        NavController navController = navHostFragment.getNavController();
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.usuarioFragment, R.id.reservaFragment, R.id.mensajesFragment
+        ).build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
-
 }
