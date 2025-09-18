0cfe28724c7c8e4c3f6e908979a38ebd11c87073
Ivan9556 <168356668+Ivan9556@users.noreply.github.com>
2025-09-18 15:41:25 +0200

    Comentarios

--- app/src/main/java/com/example/halimaapp/MenuActivity.java
+++ app/src/main/java/com/example/halimaapp/MenuActivity.java
@@ -25,22 +25,41 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MenuActivity extends AppCompatActivity {
 
+    /*
+       Uso de ViewBinding:
+       Se usa para reemplazar el clasico findViewById, el cual generar automáticamente una clase por
+       cada layout XML. Una forma segura para acceder a las vistas dentro del Layout. Por lo que
+       representa el activity_menu.xml
+     */
     private ActivityMenuBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*
+        Metodo inflate: crea en memoria el layout activity_menu.xml y devuelve la instancia
+        de la clase generada que contiene todas las vistas layout (mapeamos las vistas en el objeto
+        binding).
+         */
         binding = ActivityMenuBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(binding.getRoot()); //getRoot(): Obtiene la vista
 
+        //Barra superior del activity
         MaterialToolbar toolbar = binding.toolbar;
         setSupportActionBar(toolbar);
 
-        // Obtenemos NavController correctamente
+        /*  NavController: Maneja toda la navegación.
+            Primero obtener el host navHostFragment con getSupportFragmentManager() del XML,nreferenciado con findFragmentById()
+            para representar los distintos fragments.
+            getNavController(): Devuelve el navController asociado a NavHostFragment.
+        */
         NavHostFragment navHostFragment =
                 (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
         NavController navController = navHostFragment.getNavController();
 
+        /* AppBarConfiguration
+        Define cuales son los destinos principales y muestra el título correcto del fragmento actual.
+         */
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.usuarioFragment, R.id.reservaFragment, R.id.mensajesFragment
         ).build();
